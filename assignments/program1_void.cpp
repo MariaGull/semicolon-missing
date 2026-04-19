e7e1a703b75c8089e116032b5385c6e2d22f4e0f
MARIA <mariagull360@gmail.com>
2026-04-19 15:52:56 +0500

    Create program1_void.cpp

---
+++ assignments/program1_void.cpp
@@ -0,0 +1,24 @@
+/*Reverse Roll Numbers List
+Statement: 
+A teacher stores roll numbers of 5 students in an array. Write a C++ program that inputs 5 roll numbers from the user and passes the 
+array to a function. The function should display all roll numbers in reverse order.
+Note: Also Write the above code using return( ) method.*/
+#include<iostream>
+using namespace std;
+
+void reverse(int arr[], int n){
+    for(int i = 4; i >= 0; i--){
+        cout << arr[i] << " ";
+    }
+}
+
+int main(){
+    int arr[5];
+    cout << "Enter 5 roll numbers: "<<endl;
+    for(int i = 0; i < 5; i++){
+        cin >> arr[i];
+    }
+    cout << "Reversed: "<<endl;
+    reverse(arr, 5);
+    return 0;
+}
